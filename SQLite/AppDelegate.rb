44cc759f0414391dfc95c2e58abfa9eb7820f9e9
tk84 <git@tk84.net>
2011-10-30 21:30:59 +0900

    Initial Commit

---
+++ SQLite/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  SQLite
+#
+#  Created by Hiroyuki Takahashi on 11/10/30.
+#  Copyright 2011年 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
