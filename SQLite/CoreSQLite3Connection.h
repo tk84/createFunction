8d4e951e137d3eae56de3d9336ad567663b78036
tk84 <git@tk84.net>
2011-10-30 21:51:33 +0900

    自分用ファイル作った

---
+++ SQLite/CoreSQLite3Connection.h
@@ -0,0 +1,14 @@
+//
+//  CoreSQLite3Connection.h
+//  SQLite
+//
+//  Created by Hiroyuki Takahashi on 11/10/30.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SQLite3Connection.h"
+
+@interface SQLite3Connection (Function)
+
+@end
---
+++ SQLite/CoreSQLite3Connection.m
@@ -0,0 +1,13 @@
+//
+//  CoreSQLite3Connection.m
+//  SQLite
+//
+//  Created by Hiroyuki Takahashi on 11/10/30.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreSQLite3Connection.h"
+
+@implementation SQLite3Connection (Function)
+
+@end
